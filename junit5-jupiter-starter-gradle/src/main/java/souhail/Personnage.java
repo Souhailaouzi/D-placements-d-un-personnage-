7d8c543f8b2ba4a8815739fa996de0defa194db4
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 16:06:27 +0100

    Refactor : Généralisation avec un tableau pour toutes les directions

--- junit5-jupiter-starter-gradle/src/main/java/souhail/Personnage.java
+++ junit5-jupiter-starter-gradle/src/main/java/souhail/Personnage.java
@@ -1,15 +1,18 @@
 package souhail;
 public class Personnage{
     public static String tourner(String orientationInitiale, int fois) {
-        switch (orientationInitiale) {
-            case "EST":
-                return "SUD";
-            case "OUEST":
-                return "NORD";
-            default:
-                throw new IllegalArgumentException("Orientation inconnue : " + orientationInitiale);
+        String[] directions = {"NORD", "EST", "SUD", "OUEST"};
+        int index = java.util.Arrays.asList(directions).indexOf(orientationInitiale);
+    
+        if (index == -1) {
+            throw new IllegalArgumentException("Orientation inconnue : " + orientationInitiale);
         }
+    
+        int newIndex = (index + fois % 4 + 4) % 4;
+        return directions[newIndex];
     }
+    
+    
     
     
     
