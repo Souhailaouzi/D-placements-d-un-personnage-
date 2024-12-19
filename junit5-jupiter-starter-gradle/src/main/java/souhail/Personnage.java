3a7d65a7d5ac6064974cdbe783f6a47ab507687e
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 15:22:32 +0100

    Initialisation du projet avec la structure de base

---
+++ junit5-jupiter-starter-gradle/src/main/java/souhail/Personnage.java
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*package souhail;
+
+public class Personnage {
+
+    public static String tourner(String orientationInitiale, int fois) {
+        String[] tab = {"NORD", "EST", "SUD", "OUEST"};
+
+        int pos = -1;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i].equals(orientationInitiale)) {
+                pos = i;
+                break;
+            }
+        }
+
+        if (pos == -1) {
+            System.out.println("erreur a zbbi");
+        }
+        int newindice = (pos+ fois % 4 + 4) % 4;
+        return tab[newindice];
+    }
+}
+*/
---
+++ junit5-jupiter-starter-gradle/src/test/java/souhail/PersonnageTest.java
@@ -0,0 +1,89 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*package souhail;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PersonnageTest {
+
+    @Test
+    public void testTournerUneFois() {
+        // Given
+        String ch = "NORD";
+
+        // When
+        String res = Personnage.tourner(ch, 1);
+
+        // Then
+        assertEquals("EST", res);
+    }
+    @Test
+    public void tourner_deuxfois(){
+        //given
+        String ch = "NORD";
+        //when
+        String res = Personnage.tourner(ch, 2);
+        //then
+        assertEquals("SUD", res);
+
+    }
+
+    @Test
+    public void testTournerNegatif() {
+        // Given
+        String ch = "NORD";
+
+        // When
+        String res = Personnage.tourner(ch, -1);
+
+        // Then
+        assertEquals("OUEST", res);
+    }
+}
+*/
